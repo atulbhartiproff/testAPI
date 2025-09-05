11f67de8d9bd20df5655c15a9c54b960decb523d
atulbhartiproff <atulbhartiproff@gmail.com>
2025-09-06 04:31:49 +0530

    Made the interactions into more gamification

--- src/routes/interactions.js
+++ src/routes/interactions.js
@@ -4,15 +4,41 @@ import { db } from "../db.js"
 const router = Router();
 
 // Log user-item interaction
-router.post("/:userId", async (req, res) => {
-  const { userId } = req.params;
-  const { itemId, type, rating } = req.body;
-
+router.post("/", async (req, res) => {
+  const { user_id, game_id, action, rating} =req.body;
+  try{
   const result = await db.query(
-    "INSERT INTO interactions (user_id, item_id, type, rating) VALUES ($1, $2, $3, $4) RETURNING *",
-    [userId, itemId, type, rating]
+    `INSERT INTO interactions (user_id, game_id, action, rating)
+    VALUES ($1, $2, $3, $4) RETURNING *`,
+   [user_id, game_id, action, rating || null]
   );
   res.json(result.rows[0]);
+  }
+  catch(err)
+  {
+    console.log(err);
+    res.status(500).json({error:"Failed to log interaction"})
+  }
 });
+
+router.get("/:userid",async (req,res)=>{
+  try{
+    const {userid} =req.params;
+    const result=await db.query(
+      `SELECT i.*, g.title, g.genre, g.platform
+       FROM interactions i
+       JOIN games g ON i.game_id = g.id
+       WHERE i.user_id = $1
+       ORDER BY i.created_at DESC`,
+      [userid]
+    );
+    res.json(result.rows);
+  }
+  catch(err)
+  {
+    console.error(err);
+    res.status(500).json({error:"Failed to fetch anything"});
+  }
+})
 
 export default router;
